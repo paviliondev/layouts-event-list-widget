b64ea1fc318375b06573189a8fb70a2b64af23ed
Keegan George <kgeorge13@gmail.com>
2021-09-09 09:01:48 -0700

    FEATURE: Add user card popup for invitees

--- javascripts/discourse/widgets/layouts-event-list.js
+++ javascripts/discourse/widgets/layouts-event-list.js
@@ -125,6 +125,7 @@ createWidget("layouts-event-link", {
       const eventInviteeAvatar = h("img.l-event-invitee-avatar", {
         attributes: {
           src: invitee.user.avatar_template.replace("{size}", "40"),
+          data-user-card: invitee.user.username,
         },
       });
 
